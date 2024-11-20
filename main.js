e85e694a76206c74b9ecf589586551c5aa13182e
Vishnucoding <vishnumelitt@gmail.com>
2024-11-20 21:59:50 +0530

    calculatorv

---
+++ main.js
@@ -0,0 +1,25 @@
+// Display in calculator screen
+
+function displayContent(content){
+    result.value += content
+}
+
+//clear data
+
+function clearCalcScreen(){
+    result.value = ""
+}
+
+//backspace 
+
+function removeCalc(){
+
+    result.value = result.value.slice(0,-1)
+}
+
+// Evaluation
+
+function calcout(){
+
+    result.value = eval(result.value)
+}
